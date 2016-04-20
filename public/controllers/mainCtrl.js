a528e86934e4751e161e1db578b3d54a06e03c34
Ivan <ivan.baena04@gmail.com>
2016-04-20 00:36:52 -0400

    added styles to navigation

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -1,20 +1,25 @@
 var mainCtrl = angular.module('mainCtrl', ['ngAnimate']);
 
 //SIDENAV CONTROLLER TO HANDLE LOGIC FOR TOGGLING VIEW
-mainCtrl.controller('sideNavCtrl', ['$scope', '$http', function($scope, $http) {
+mainCtrl.controller('sideNavCtrl', ['$scope', '$http','$timeout', function($scope, $http, $timeout) {
+  $scope.fadeInContent = false;
+  $scope.fadeOutContent = true;
   $scope.showNav = false;
   $scope.activeNav = function() {
     $scope.showNav = !$scope.showNav;
+    $scope.fadeInContent = !$scope.fadeInContent;
   };
 }]);
 
 //SHOWCASE CONTROLLER
 mainCtrl.controller('showCaseCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.fadeInProject = false;
   $http.get('public/js/projects.json').then(function(data) {
     $scope.projects = data.data;
   });
   $scope.showLanguage = function(title) {
     $scope.query = title;
+    $scope.fadeInProject = !$scope.fadeInProject;
   };
 }]);
 
--- public/js/bootquery.js
+++ public/js/bootquery.js
@@ -1,1 +1,35 @@
-$('.carousel').carousel();
+var props = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' '), //define css junk
+  prop,
+  el = document.createElement('div');
+
+for (var i = 0, l = props.length; i < l; i++) {
+  if (typeof el.style[props[i]] !== "undefined") {
+    prop = props[i];
+    break;
+  }
+}
+
+var xAngle = 0,
+  yAngle = 0;
+$('body').keydown(function(evt) {
+  switch (evt.keyCode) {
+    case 37: //THIS IS LEFT
+      yAngle -= 90;
+      break;
+
+    case 38: //THIS IS UP
+      xAngle += 90;
+      evt.preventDefault();
+      break;
+
+    case 39: //THIS IS RIGHT
+      yAngle += 90;
+      break;
+
+    case 40: //THIS IS DOWN...
+      xAngle -= 90;
+      evt.preventDefault();
+      break;
+  }
+  document.getElementById('cube').style[prop] = "rotateX(" + xAngle + "deg) rotateY(" + yAngle + "deg)";
+});
